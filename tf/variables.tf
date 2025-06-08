b14f7c8a1868c96f9b6cec34271ef85d607674fe
MarcinK <marcin.kantyka@gmail.com>
2025-06-08 18:20:17 +0200

    move token to hcl

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,13 +1,4 @@
-variable "hcloud_token" {
-  description = "Hetzner Cloud API Token"
-  type        = string
-  sensitive   = true
 
-  validation {
-    condition     = length(var.hcloud_token) > 0
-    error_message = "The hcloud_token variable must not be empty."
-  }
-}
 
 variable "firewall_rules" {
   description = "values for the firewall rules to be applied"
