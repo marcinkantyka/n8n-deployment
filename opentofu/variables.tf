5f08ff19bce800e7c54705c6314b23d32e9bb437
MarcinK <marcin.kantyka@gmail.com>
2025-06-08 13:24:03 +0200

    change in env

--- opentofu/variables.tf
+++ opentofu/variables.tf
@@ -1,5 +1,11 @@
+variable "hcloud_token" {
+  description = "Hetzner Cloud API Token"
+  type        = string
+  sensitive   = true
+}
 
 variable "firewall_rules" {
+  description = "values for the firewall rules to be applied"
   type = list(object({
     protocol   = string
     port       = string
